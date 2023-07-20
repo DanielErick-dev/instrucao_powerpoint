9f6e1ef9c57a9a6176d0d3a6383f5a65e0d49c1a
Daniel Erick <DanielErick.estudante@gmail.com>
2023-07-20 10:46:28 -0300

    Initial commit

---
+++ app_excel/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def topico1(request):
+    return render(request, 'topico1.html')
+
+def topico2(request):
+    return render(request, 'topico2.html')
+
+def topico3(request):
+    return render(request, 'topico3.html')
+
+def topico4(request):
+    return render(request, 'topico4.html')
+
+def topico5(request):
+    return render(request, 'topico5.html')
+
+def topico6(request):
+    return render(request, 'topico6.html')
