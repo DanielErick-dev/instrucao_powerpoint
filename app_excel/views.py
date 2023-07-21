5ad9e8c8ebddee848e5b5e05f2cb02061d0c4da5
Daniel Erick <DanielErick.estudante@gmail.com>
2023-07-20 22:41:48 -0300

    salvando

--- app_excel/views.py
+++ app_excel/views.py
@@ -20,3 +20,36 @@ def topico5(request):
 
 def topico6(request):
     return render(request, 'topico6.html')
+
+def topico7(request):
+    return render(request, 'topico7.html')
+
+def topico8(request):
+    return render(request, 'topico8.html')
+
+def topico9(request):
+    return render(request, 'topico9.html')
+
+def topico10(request):
+    return render(request, 'topico10.html')
+
+def topico11(request):
+    return render(request, 'topico11.html')
+def topico12(request):
+    return render(request, 'topico12.html')
+def topico13(request):
+    return render(request, 'topico13.html')
+def topico14(request):
+    return render(request, 'topico14.html')
+def topico15(request):
+    return render(request, 'topico15.html')
+def topico16(request):
+    return render(request, 'topico16.html')
+def topico17(request):
+    return render(request, 'topico17.html')
+def topico18(request):
+    return render(request, 'topico18.html')
+def topico19(request):
+    return render(request, 'topico19.html')
+def topico20(request):
+    return render(request, 'topico20.html')
